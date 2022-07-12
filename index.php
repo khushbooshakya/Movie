09a385f1dc694b89488c5ca1c6edfebb77b93b01
khushbooshakya <khushishakya0410@gmail.com>
2022-07-12 08:51:28 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="form_pg">
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	
+     	<label>User Name</label>
+     	<input type="text" name="uname" placeholder="User Name"><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+		<p>
+		<button><a href="signup.php" >Sign Up</a></button>
+		<button><a href="home.php" >Login</a></button>
+		</p>
+     </form>
+</body>
+</html>
